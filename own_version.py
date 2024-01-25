c32b3eb80a744215229271f0ae7da33bea4348f7
katwar0809 <katwar0809@highgateschool.org.uk>
2024-01-25 12:35:49 +0000

    In progress DJN 240125

--- own_version.py
+++ own_version.py
@@ -1,6 +1,9 @@
 import pygame
 import math
 
+e = 0.7
+# coefficient of restitution
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (64, 64, 64)
@@ -12,7 +15,7 @@ window = pygame.display.set_mode((wScreen, hScreen))
 
 class Particle(object):
     def __init__(self, x, y, radius):
-        #self.init_vel = None
+        # self.init_vel = None
         self.x = x
         self.y = y
         self.radius = radius
@@ -20,6 +23,8 @@ class Particle(object):
         self.uY = 0
         self.vX = 0
         self.vY = 0
+        self.acc = -9.8
+        self.t = 0
 
     def create(self):
         # changed name from draw to create so i wouldn't get confused w the pygame draw function
@@ -29,88 +34,33 @@ class Particle(object):
         line_length = pygame.mouse.get_pos()[0] - particle.x
         line_height = particle.y - pygame.mouse.get_pos()[1]
 
-        self.uX = line_length/8
-        self.uY = line_height/8
+        self.uX = line_length / 5
+        self.uY = line_height / 5
 
-        #hyp = math.sqrt((line_length ** 2) + (line_height ** 2))
+    def findBounces(self):
+        bounce_num = math.log(2/abs(self.uY),e)
 
-        #self.init_vel = hyp / 8
+        return bounce_num
 
+    def path(self, initX, initY, t):
 
-    def path(self, initX, initY, angle, t):
-        #uX = init_vel * math.cos(angle)
-        #uY = init_vel * math.sin(angle)
+        if t == 0:
+            self.vX, self.vY = self.uX, self.vY
+            self.x, self.y = initX, initY
 
-        sX = self.uX * t
-        sY = (self.uY * t) + (-4.9 * (t ** 2))
+        else:
+            sX = self.uX * t
+            sY = (self.uY * t) + (self.acc/2 * (t ** 2))
+            self.vX = self.uX
+            self.vY = self.uY + self.acc * t
+            self.x, self.y = initX + sX, initY - sY
 
-        self.vX = self.uX
-        self.vY = (sY + (-4.9*(t**2)))/t
+    def bounceOffParabola(self):
+        # Check if the particle collides with the parabolic curve
+        if self.y >= -0.0005 * (self.x - wScreen / 2) ** 2 + (hScreen-50):
+            # Calculate the new velocity after the bounce
+            self.uY = -e * self.vY
 
-        new_position = (initX + sX, initY - sY)
-
-        return new_position
-
-    def checkCollision(self, boundary):
-        collision = False
-        boundary_vector = boundary.vector
-
-        particle_vector = (particle.x - boundary.pt1[0]), (particle.y - boundary.pt1[1])
-        particle_magnitude = math.sqrt((particle_vector[0])**2 + (particle_vector[1])**2)
-
-        # finding l:
-        dot_product = (boundary_vector[0] * particle_vector[0]) + (boundary_vector[1] * particle_vector[1])
-        angle = math.acos(dot_product / (boundary.magnitude * particle_magnitude))
-
-        distance_to_wall = math.sin(angle) * particle_magnitude
-
-        # checking if collision:
-        if distance_to_wall < particle.radius:
-            collision = True
-
-        return collision
-
-
-class Boundary(object):
-    def __init__(self, pt1, pt2):
-        self.pt1 = pt1
-        self.pt2 = pt2
-        self.vector = (pt2[0]-pt1[0]), (pt2[1]-pt1[1])
-        self.magnitude = math.sqrt((self.vector[0])**2 + (self.vector[1])**2)
-
-    def create(self):
-        pygame.draw.line(window,BLACK,self.pt1,self.pt2,1)
-
-
-def findAngle(mouse_pos):
-    start_x = particle.x
-    start_y = particle.y
-    mouse_x = mouse_pos[0]
-    mouse_y = mouse_pos[1]
-
-    if start_x - mouse_x == 0:
-        angle = math.pi / 2
-
-    else:
-        angle = math.atan((start_y - mouse_y) / (start_x - mouse_x))
-
-    if start_x > mouse_x and mouse_y > start_y:
-        # bottom left quad
-        angle = math.pi + abs(angle)
-
-    elif start_x > mouse_x and start_y > mouse_y:
-        # top left quad
-        angle = math.pi - angle
-
-    elif mouse_x > start_x and mouse_y > start_y:
-        # bottom right quad
-        angle = (math.pi*2) - angle
-
-    elif mouse_x > start_x and start_y > mouse_y:
-        # top right quad
-        angle = abs(angle)
-
-    return angle
 
 def drawArrow(particle_pos, mouse_pos, arrowhead_length):
     pygame.draw.line(window, WHITE, particle_pos, mouse_pos, 1)
@@ -133,59 +83,79 @@ def drawArrow(particle_pos, mouse_pos, arrowhead_length):
     pygame.draw.line(window, WHITE, mouse_pos, arrowhead_points[2], 1)
 
 
+def drawParabola():
+    for x_coord in range(wScreen):
+        y_coord = -0.0005 * (x_coord - wScreen / 2) ** 2 + 0 * (x_coord - wScreen / 2) + hScreen - 50
+        pygame.draw.circle(window, BLACK, (x_coord, int(y_coord)), 1)
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CORE CODE: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 
-particle = Particle(wScreen/2, hScreen/2, 10)
+particle = Particle(wScreen / 2, hScreen / 2, 10)
 projected = False
 clock = pygame.time.Clock()
 ground = hScreen - particle.radius
 
 time = 0
 
-walls = [Boundary((0,0),(0,hScreen)), Boundary((0,0),(wScreen,0)), Boundary((wScreen,0),(wScreen,hScreen))]
+
 
 def redrawWindow():
     window.fill(GREY)
+    drawParabola()
     particle.create()
     drawArrow((particle.x, particle.y), pygame.mouse.get_pos(), 15)
     pygame.display.update()
 
 
 running = True
-collision = False
 
 while running:
     clock.tick(400)
 
     if projected:
-
-        if particle.y < ground:
-            # only runs if particle is still in the air
+        if bounces < max_bounces:
 
             time += 0.05
 
-            # testing collisions without actually using collision method j to see if it works normally
+            particle.bounceOffParabola()
 
-            if particle.x <= 0 or particle.x >= wScreen:
-                particle.uX = -particle.vX
+            if particle.x <= particle.radius:
+                x, y = particle.x + particle.radius, particle.y
+                time = 0
+                bounces += 1
+                particle.uY = e * particle.vY
+                particle.uX = -e * particle.vX
 
-            if particle.y <= 0 or particle.y >= hScreen:
-                particle.uY = -particle.vY
+            if particle.x >= wScreen - particle.radius:
+                x, y = particle.x - particle.radius, particle.y
+                time = 0
+                bounces += 1
+                particle.uY = e * particle.vY
+                particle.uX = -e * particle.vX
 
-            projection_path = particle.path(x, y, theta, time)
+            if particle.y <= particle.radius:
+                x, y = particle.x, particle.y + particle.radius
+                time = 0
+                bounces += 1
+                particle.uY = -e * particle.vY
+                particle.uX = e * particle.vX
+            '''
+            if particle.y >= hScreen - particle.radius + 1:
+                x, y = particle.x, particle.y - particle.radius
+                time = 0
+                bounces += 1
+                particle.uX = e * particle.vX
+                particle.uY = -e * particle.vY
+            '''
 
-
-            particle.x = projection_path[0]
-            particle.y = projection_path[1]
-
+            particle.path(x,y,time)
 
         else:
             projected = False
-            particle.y = ground - 1
-            time = 0
-
+            particle.y = particle.y
+            particle.x = x
 
     redrawWindow()
 
@@ -196,10 +166,10 @@ while running:
             if not projected:
                 x = particle.x
                 y = particle.y
-                theta = findAngle(pygame.mouse.get_pos())
                 particle.findInitVel()
+                bounces = 0
+                max_bounces = particle.findBounces()
                 projected = True
             # checks that particle isn't already being projected. if not, then particle is projected.
-
 
 pygame.quit()
